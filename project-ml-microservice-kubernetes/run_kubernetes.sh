adeb65ea3be5783aceb48f0aff1f4b74265953a9
Daniel Dominguez <daniel@Daniels-MBP.attlocal.net>
2023-04-17 10:53:55 -0500

    project submission

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ddanny99/udacity-ml-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-ml-project --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
+
+sleep 40
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-ml-project 8000:80
